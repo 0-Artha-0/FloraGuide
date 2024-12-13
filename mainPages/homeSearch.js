cdbc943c1efa9f0a1116dd4153229fdab01cf67f
NadiaMohamed1739 <169031998+NadiaMohamed1739@users.noreply.github.com>
2024-12-14 00:38:45 +0400

    Update homeSearch.js

--- mainPages/homeSearch.js
+++ mainPages/homeSearch.js
@@ -1,1 +1,23 @@
 
+// start function called when page loads
+function start() {
+    // get search button id
+    var submitBtn = document.getElementById("search");
+    submitBtn.addEventListener("click", send, false);
+}
+
+function send() {
+    // get search input
+    var searchInput = document.getElementById("plantSearch");
+    var searchTerm = searchInput.value;
+
+    // if there is a search input go to the directory page
+    if (searchTerm.trim()) {
+        window.location.href = "directory.html";
+    }
+    else {
+        alert("Please enter a search term.");
+    }
+}
+
+window.addEventListener("load", start, false);
