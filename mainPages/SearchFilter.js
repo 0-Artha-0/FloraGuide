a02284fbd4b29e85b374509794b42c3ea51e9751
Aya-Arnaout <petitemya111@gmail.com>
2024-12-13 16:44:00 +0400

    Update SearchFilter.js

--- mainPages/SearchFilter.js
+++ mainPages/SearchFilter.js
@@ -1,1 +1,34 @@
+// start function called when page loads
+function start() {
+    // get search button id
+    var submitBtn = document.getElementById("search");
+    submitBtn.addEventListener("click", filter, false);
+}
 
+// filter function called when user inputs in search bar
+function filter() {
+    // get search input
+    var searchInput = document.getElementById("plantSearch");
+    var searchTerm = searchInput.value.toLowerCase();
+
+    // get all plant items
+    var plantList = document.getElementById("plantList");
+    var plantItems = plantList.getElementsByTagName("li");
+
+    // Loop through all plant list items
+    var len = plantItems.length;
+    for (var i = 0; i < len; i++) {
+        var plant = plantItems[i];
+        var link = plant.querySelector("a");
+        var plantL = link.innerHTML.toLowerCase();
+
+        // Show/hide plant items based on search term
+        if (plantL.includes(searchTerm)) {
+            plant.style.display = "";
+        }
+        else {
+            plant.style.display = "none";
+        }
+    }
+}
+window.addEventListener("load", start, false);
