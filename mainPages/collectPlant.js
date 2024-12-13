5a74b5fbac9147120dfda0f07baf2674ad4ea649
artha <U21101466@sharjah.ac.ae>
2024-12-13 23:22:49 +0400

    Update collectPlant.js

--- mainPages/collectPlant.js
+++ mainPages/collectPlant.js
@@ -1,22 +1,63 @@
-// start function called when page loads
+// constant for plant stickers collection
+const plantStickers = ["aloe.png", "flamingo.png", "arecapalm.png", "birdnestfern.png",
+    "bostonfern.png", "calathea.png", "castiron.png", "evergreen.png", "moneyplant.png",
+    "croton.png", "dumbcane.png", "dracaena.png", "englishivy.png", "fiddle.png",
+    "maidenhair.png", "monstera.png", "parlorpalm.png", "peace.png", "peperomia.png",
+    "philodendron.png", "ponytailpalm.png", "pothos.png", "prayerplant.png", "rubber,png",
+    "snake.png", "spider.png", "pearls.png", "swisscheese.png", "zz.png"];
+
+const plantNames = ["Aloe", "Flamingo", "Areca", "Bird's", "Boston",
+    "Calathea", "Cast Iron", "Chinese Evergreen", "Money Plant", "Croton",
+    "Dumb Cane", "Dracaena", "English Ivy", "Fiddle", "Maidenhair",
+    "Monstera deliciosa", "Parlor", "Peace", "Peperomia", "Philodendron",
+    "Ponytail", "Pothos", "Prayer", "Rubber", "Snake", "Spider", "String",
+    "Swiss", "ZZ Plant"];
+
+// function start when page loads 
 function start() {
-    // get search button id
-    var submitBtn = document.getElementById("search");
-    submitBtn.addEventListener("click", send, false);
+    var collection = JSON.parse(localStorage.getItem("collections")) || [];
+    console.log(collection);
+
+    var collectBtn = document.getElementById("collectBtn");
+    collectBtn.addEventListener("click", addPlant, false);
 }
 
-function send() {
-    // get search input
-    var searchInput = document.getElementById("plantSearch");
-    var searchTerm = searchInput.value;
+function addPlant() {
+    // get the plant name of the caller page
+    var plantName = document.getElementById("names");
+    var plant = plantName.innerText;
 
-    // if there is a search input go to the directory page
-    if (searchTerm.trim()) {
-        window.location.href = "directory.html";
+    // get the existing collection list from the browser local storage
+    // or make a new one if there's none
+    var collection = JSON.parse(localStorage.getItem("collections")) || [];
+
+    // loop and check plant name to determine image location to add to the collection
+    for (var i = 0; i < plantNames.length; i++) {
+        if (plant.includes(plantNames[i])) {
+            var notsaved = true;
+
+            // check if plant is already added to the collection
+            for (var j of collection) {
+                if (j === plantStickers[i]) {
+                    alert("Plant already added.");
+                    notsaved = false;
+                    break;
+                }
+            }
+
+            // if plant was not added peviously, add it to the collection
+            if (notsaved) {
+                collection.push(plantStickers[i]);
+                localStorage.setItem("collections", JSON.stringify(collection));
+
+                alert("Plant added to collection successfully!");
+                break;
+            }
+
+        }
+
     }
-    else {
-        alert("Please enter a search term.");
-    }
+
 }
 
 window.addEventListener("load", start, false);
